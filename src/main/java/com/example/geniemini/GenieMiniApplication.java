cf87e6b7f3337f1adccbdccde11a21d27397519a
KetanJain132 <ketanjain132@gmail.com>
2024-09-08 19:03:49 +0530

    Spring Initializr Setup

---
+++ src/main/java/com/example/geniemini/GenieMiniApplication.java
@@ -0,0 +1,13 @@
+package com.example.geniemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenieMiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenieMiniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/geniemini/GenieMiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.geniemini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenieMiniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
